93b963e3cafb5df31fc4d540b49d4ebe1cf589e2
geoluread <arlyn.harris2013@gmail.com>
2022-01-23 22:41:20 -0500

    updated with my interests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Website
-A mock up of my website.
+Updated: January 23. 2022
+By: Geoulread
+
+A mock up of my website. It incorporates some mustache, and confetti.js for the styling and the templating. 
--- arlynjs.js
+++ arlynjs.js
@@ -11,22 +11,61 @@ function stopConfetti(){
 //When button is pressed the confetti falls down
 function topButton(data){
 
-    //Verifies the button has been clicked.
-    console.log("Button clicked");
+    if(document.getElementById("info").style.display === "none")
+    {
+        document.getElementById("info").style.display = "block";
+    }
     //Makes the confetti visible and allows it to be visible for 3 seconds.
     document.getElementById("my-canvas").style.visibility = "visible";
     setTimeout(stopConfetti, 3000);
 
+    //Retrieve JSON to fill the mustache template.
+    $.getJSON('resumeInterestProjects.json', function(result) {
+
     //If the resume button was clicked the following will execute the data into the mustache template.
     if(data==="resume"){
-
-        console.log("Resume has been clicked");
-        document.getElementById("info").style.visibility = "visible";
-        $.getJSON('resumeInterestProjects.json', function(result) {
-            var template = $("#template_must").html();
-            var text = Mustache.render(template, result);   
-            $("#info").html(text);
-        });
+        outputResume(result);
     }
+    else if(data == "project"){
+        outputProject(result);
+    }
+    else if(data = "interest"){
+        outputInterest(result);
+    }
+    });
 }
 
+
+function outputResume(result){
+
+    console.log("Resume has been clicked");
+    document.getElementById("info").style.visibility = "visible";
+    var template = $("#template_resume").html();
+    var text = Mustache.render(template, result);   
+    $("#info").html(text);
+}
+
+function outputProject(result){
+
+    console.log("Project has been clicked");
+    document.getElementById("info").style.visibility = "visible";
+    var template = $("#template_project").html();
+    var text = Mustache.render(template, result);   
+    $("#info").html(text);
+}
+
+function outputInterest(result){
+
+    console.log("Interest has been clicked");
+    document.getElementById("info").style.visibility = "visible";
+    var template = $("#template_interest").html();
+    var text = Mustache.render(template, result);   
+    $("#info").html(text);
+}
+
+function hide(){
+
+    document.getElementById("info").style.display = "none";
+    document.getElementById("my-canvas").style.visibility = "visible";
+    setTimeout(stopConfetti, 3000);
+}
